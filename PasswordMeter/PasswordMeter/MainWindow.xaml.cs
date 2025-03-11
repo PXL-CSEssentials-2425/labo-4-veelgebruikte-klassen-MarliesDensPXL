5a4f189259e0df9d5fc16aeaa757084903bac22c
Marlies Dens <marlies.dens@pxl.be>
2025-03-11 20:04:04 +0100

    wachtwoord meten

--- PasswordMeter/PasswordMeter/MainWindow.xaml.cs
+++ PasswordMeter/PasswordMeter/MainWindow.xaml.cs
@@ -35,6 +35,59 @@ namespace PasswordMeter
         private void passwordMeterButton_Click(object sender, RoutedEventArgs e)
         {
             resultTextBlock.Foreground = Brushes.Red;
+            string username = userNameTextBox.Text.Trim();
+            string password = passwordTextBox.Text.Trim();
+
+            // drie voorwaarden voor sterk wachtwoord
+            bool passwordLength = password.Length >= 10;
+            bool containsDigit = password.Any(char.IsDigit);
+            bool containsUsername = !password.Contains(username);
+
+            if (passwordLength && containsDigit && containsUsername) // aan drie voorwaarden voldaan 
+                resultTextBlock.Text = "Dit is een sterk wachtwoord";
+            else if
+                ((passwordLength && containsDigit) || (passwordLength && containsUsername) || (containsDigit && containsUsername)) // aan twee voorwaarden voldaan
+                resultTextBlock.Text = "Dit wachtwoord is oké";
+            else
+            {
+                resultTextBlock.Text = "Wachtwoord is zwak";
+                Random rnd = new Random();
+                StringBuilder sb = new StringBuilder();
+
+                //5 willekeurige letters
+                for (int i = 1; i <= 5; i++)
+                {
+                    int lengteUserName = userNameTextBox.Text.Length;
+                    int nummerke = rnd.Next(0, lengteUserName);
+                    string letterke = userNameTextBox.Text.Substring(nummerke, 1);
+                    sb.Append(letterke.ToLower());
+                    //sb.Append(UserNameTextBox.Text.Substring(rnd.Next(0, UserNameTextBox.Text.Length), 1).ToLower());
+                }
+
+                //5 willekeurige cijfers
+                for (int i = 1; i <= 5; i++)
+                {
+                    sb.Append(rnd.Next(0, 11));
+                }
+
+                //Hoofdletters toevoegen
+                for (int i = 1; i <= 2; i++)
+                {
+                    sb.Append(userNameTextBox.Text.Substring(rnd.Next(0, userNameTextBox.Text.Length), 1).ToUpper());
+                }
+
+                //Uitroeptekens
+                int randomGetalleke = rnd.Next(1, 6);
+                for (int i = 1; i <= randomGetalleke; i++)
+                {
+                    sb.Append('!');
+                }
+
+                if (MessageBox.Show($"Nieuw wachtwoord voorstelling: {sb.ToString()}", "Akkoord?", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    passwordTextBox.Text = sb.ToString();
+                }
+            }
         }
     }
 }
